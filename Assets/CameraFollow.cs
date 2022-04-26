d96e4e42230615b3e7094058a0ebf47155bb6a3e
Mohamed <mohamedboudissa69009@gmail.com>
2022-04-26 12:35:26 +0200

    ajout de limites de map

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -8,10 +8,28 @@ public class CameraFollow : MonoBehaviour
     public float yOffset = 1f;
     public Transform target;
 
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
     // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+
+
+
+        transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+                Mathf.Clamp(transform.position.x, bottomLimit, topLimit),
+                transform.position.z
+            );
     }
 }
